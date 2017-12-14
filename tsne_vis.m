bda4b71e78e29a8fee692a48c57886a41d06cdfb
Justin Pinkney <justinpinkney@gmail.com>
2017-12-14 21:26:44 +0000

    tsne vis testing

---
+++ tsne_vis.m
@@ -0,0 +1,34 @@
+nImages = 10;
+nRegions = 100;
+
+
+allFeatures = vertcat(imageDb.Images.Features);
+Y = tsne(allFeatures(1:nImages*nRegions,:));
+figure;
+scatter(Y(:,1), Y(:,2), 20, repelem((1:nImages)', nRegions), 'filled')
+
+Y = Y./range(Y);
+Y = Y - min(Y);
+outSize = 2000;
+pad = 40;
+bigIm = zeros(outSize + 2*pad, outSize + 2*pad, 3, 'uint8');
+imSize = 40;
+%%
+for iIm = 1:nImages
+    disp(iIm);
+    regions = imageDb.Images(iIm).Regions;
+    im = imageDb.Images(iIm).Image;
+    nRegions = size(regions, 1);
+    %imCrops = zeros(imSize, imSize, 3, nRegions, 'uint8');
+    for iRegion = 1:nRegions
+        imCrop = imcrop(im, regions(iRegion, :));
+        imSmall = imresize(imCrop, [imSize, imSize]);
+        xStart = pad + round(outSize*Y(nRegions*(iIm-1) + iRegion, 1));
+        yStart = pad + round(outSize*Y(nRegions*(iIm-1) + iRegion, 2));
+        bigIm(xStart:xStart + imSize - 1, ...
+                yStart:yStart + imSize - 1, :) = imSmall;
+    end
+end
+
+figure
+imshow(bigIm)
