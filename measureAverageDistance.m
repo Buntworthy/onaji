997e0f983786d3646af25db91045eecce2f20bd1
Justin Pinkney <justinpinkney@gmail.com>
2017-12-14 20:42:22 +0000

    TIdy

--- measureAverageDistance.m
+++
@@ -1,9 +0,0 @@
-function averageDistance = measureAverageDistance(query, features)
-
-    averageDistance = zeros(numel(features), 1);
-    
-    for iIm = 1:numel(features)
-        iDistances = pdist2(reshape(query, 1, 4096), features{iIm}, 'cosine');
-        averageDistance(iIm) = mean(iDistances);
-    end
-end
--- test.m
+++
@@ -1,6 +0,0 @@
-%% Display filenames
-
-
-%% Display bboxes
-% input table of files and bboxes, different figure for each class
-Gallery(labelData(:, 1:2));
