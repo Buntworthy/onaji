8ef7d04d6dbd8cc1891df3ccd90c2b5f2fa8a5f0
Justin Pinkney <justinpinkney@gmail.com>
2017-12-14 21:03:23 +0000

    EdgeBoxes as external package

--- EdgeBoxRegionProposer.m
+++ EdgeBoxRegionProposer.m
@@ -11,16 +11,13 @@ classdef EdgeBoxRegionProposer < RegionProposer
         function this = EdgeBoxRegionProposer(nRegions)
             this.NRegions = nRegions;
             
-            addpath(genpath('C:\Software\toolbox'));
-            addpath(genpath('C:\Software\edges'));
-
-            model = load('C:/Software/edges/models/forest/modelBsds'); 
+            model = load('+extern/models/forest/modelBsds'); 
             this.Model = model.model;                        
             this.Model.opts.multiscale = 0; 
             this.Model.opts.sharpen = 0; 
             this.Model.opts.nThreads = 4;
             
-            this.Opts = edgeBoxes;
+            this.Opts = extern.edgeBoxes;
             this.Opts.alpha = 0.625;     % step size of sliding window search
             this.Opts.beta = 0.75;     % nms threshold for object proposals
             this.Opts.minScore = 0.02;  % min score of boxes to detect
@@ -30,7 +27,7 @@ classdef EdgeBoxRegionProposer < RegionProposer
         
         function regions = calculate(this, im)
             im = imresize(im, 1);
-            edgeBoxOutput = edgeBoxes(im, this.Model, this.Opts);
+            edgeBoxOutput = extern.edgeBoxes(im, this.Model, this.Opts);
             regions = 1*edgeBoxOutput(:, 1:4);
         end
         
